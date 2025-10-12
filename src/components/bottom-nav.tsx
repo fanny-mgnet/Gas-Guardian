435a669f198cb3a257815df669ec0ae3286fc00e
Adorable World <manmattu1@gmail.com>
2025-10-12 08:39:47 +0000

    unable to see the bottom navigation

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -9,7 +9,7 @@ export function BottomNav() {
   const pathname = usePathname();
 
   return (
-    <div className="md:hidden sticky bottom-0 left-0 z-50 w-full h-16 bg-card border-t">
+    <div className="sticky bottom-0 left-0 z-50 w-full h-16 bg-card border-t">
       <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
         {navItems.map((item) => (
           <Link
